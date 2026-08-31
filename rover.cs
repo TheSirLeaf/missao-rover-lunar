32f16d3a92d8e708d58e18158b63ab5d2dbb027f
Patrick <patrick.brito@jssjcompany.com>
2026-08-31 20:33:36 -0300

    feat: Add initial implementation of lunar rover program

---
+++ rover.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Rover lunar inicializado.");
+        Console.WriteLine("Digite algo para continuar...");
+        Console.ReadLine();
+    }
+}
